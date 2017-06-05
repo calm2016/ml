ef3fb76dba3d5820f314ee795f6682f69f0c64ab
Jacky Cheng <jacky.cheng@sap.com>
2017-06-05 10:05:28 +0800

    sk polynomial linear regression

--- 6.Data/study/linear-regression-3x.py
+++ 6.Data/study/linear-regression-3x.py
@@ -14,40 +14,40 @@ quadratic_featurizer = PolynomialFeatures(degree=2)
 
 
 # Use only one feature
-diabetes_X = np.arange(0, 10, 0.1).reshape(-1, 1)
-diabetes_Y = 3*diabetes_X
-# diabetes_X = quadratic_featurizer.fit_transform(diabetes_X)
-# print diabetes_X
-# print diabetes_Y
+data_X = np.arange(0, 10, 0.1).reshape(-1, 1)
+data_Y = 3*data_X
+# data_X = quadratic_featurizer.fit_transform(data_X)
+# print data_X
+# print data_Y
 
 # Split the data into training/testing sets
-diabetes_X_train = diabetes_X[:-20]
-diabetes_X_test = diabetes_X[-20:]
+data_X_train = data_X[:-20]
+data_X_test = data_X[-20:]
 #
 # Split the targets into training/testing sets
-diabetes_y_train = diabetes_Y[:-20]
-diabetes_y_test = diabetes_Y[-20:]
-print diabetes_y_train, diabetes_y_test
+data_y_train = data_Y[:-20]
+data_y_test = data_Y[-20:]
+print data_y_train, data_y_test
 #
-plt.plot(diabetes_X, diabetes_Y)
+plt.plot(data_X, data_Y)
 
 # # # Create linear regression object
 regr = linear_model.LinearRegression()
 
 # Train the model using the training sets
-regr.fit(diabetes_X_train, diabetes_y_train)
+regr.fit(data_X_train, data_y_train)
 
 # The coefficients
 print('Coefficients: \n', regr.coef_)
 # The mean squared error
 print("Mean squared error: %.2f"
-      % np.mean((regr.predict(diabetes_X_test) - diabetes_y_test) ** 2))
+      % np.mean((regr.predict(data_X_test) - data_y_test) ** 2))
 # Explained variance score: 1 is perfect prediction
-print('Variance score: %.2f' % regr.score(diabetes_X_test, diabetes_y_test))
+print('Variance score: %.2f' % regr.score(data_X_test, data_y_test))
 
 # Plot outputs
-plt.scatter(diabetes_X_test, diabetes_y_test,  color='black')
-plt.plot(diabetes_X_test, regr.predict(diabetes_X_test), color='blue',
+plt.scatter(data_X_test, data_y_test,  color='black')
+plt.plot(data_X_test, regr.predict(data_X_test), color='blue',
          linewidth=3)
 
 # plt.xticks(())
