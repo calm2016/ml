706533f15b8ea43d6052cca4a6e861c69e4b227b
Jacky Cheng <jacky.cheng@sap.com>
2017-06-21 13:46:34 +0800

    logistic regress, random forest, svm, decision tree

--- 11.RandomForest/study/LogisticRegressionIris.py
+++ 11.RandomForest/study/LogisticRegressionIris.py
@@ -5,6 +5,8 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
+from sklearn import svm
+from sklearn.ensemble import RandomForestClassifier
 
 iris = datasets.load_iris()
 x1 = iris.data[:, 0]
@@ -30,3 +32,17 @@ tree.fit(x_train, y_train)
 y_hat = tree.predict(x_test)
 result = (100 * np.mean(y_hat == y_test.ravel()))
 print "DecisionTreeClassifier accuracy:", result
+
+rfc = RandomForestClassifier(n_estimators=25)
+rfc.fit(x_train, y_train)
+y_hat = rfc.predict(x_test)
+result = (100 * np.mean(y_hat == y_test.ravel()))
+print "RandomForestClassifier accuracy:", result
+
+C = 1.0  # SVM regularization parameter
+clf = svm.SVC(kernel='linear', C=C)
+clf.fit(x_train, y_train)
+y_hat = clf.predict(x_test)
+# print np.stack((y_hat, y_test), -1)
+result = (100 * np.mean(y_hat == y_test.ravel()))
+print "SVM accuracy:", result
